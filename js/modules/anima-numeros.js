45157cb9479c4101a989de59ca2e07c64b9cc9da
Lucas Furtile <furtilelucas@gmail.com>
2021-05-24 18:07:38 -0300

    adicionado package-lock.json

--- js/modules/anima-numeros.js
+++ js/modules/anima-numeros.js
@@ -1,32 +1,31 @@
 export default function initAnimaNumeros() {
+  function animaNumeros() {
     const numeros = document.querySelectorAll('[data-numero]');
 
-    function AnimaNumeros() {
-        numeros.forEach((numero) => {
-            const total = +numero.innerText;
-            const incremento = Math.floor(total / 103);
-            let start = 0;
-            const timer = setInterval(() => {
-                start += incremento;
-                numero.innerText = start;
-                if (start > total) {
-                    numero.innerText = total;
-                    clearInterval(timer);
-                }
-            }, 25);
-        });
-    }
-
-    let observer;
-    function handleMutation(mutation) {
-        if (mutation[0].target.classList.contains('ativo')) {
-            observer.disconnect();
-            AnimaNumeros();
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const inscremento = Math.floor(total / 100);
+      let start = 0;
+      const timer = setInterval(() => {
+        start += inscremento;
+        numero.innerText = start;
+        if (start > total) {
+          numero.innerText = total;
+          clearInterval(timer);
         }
+      }, 25 * Math.random());
+    });
+  }
+
+  let observer;
+  function handleMutation(mutation) {
+    if (mutation[0].target.classList.contains('ativo')) {
+      observer.disconnect();
+      animaNumeros();
     }
-    observer = new MutationObserver(handleMutation);
+  }
+  observer = new MutationObserver(handleMutation);
 
-    const observerTarget = document.querySelector('.numeros');
-
-    observer.observe(observerTarget, { attributes: true });
+  const observerTarget = document.querySelector('.numeros');
+  observer.observe(observerTarget, { attributes: true });
 }

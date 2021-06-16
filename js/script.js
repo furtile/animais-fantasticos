9c2d822b2c45f90262487a06ba3a0bac4afdbdb6
Lucas Furtile <furtilelucas@gmail.com>
2021-06-15 23:40:33 -0300

    acertado caminho da api

--- js/script.js
+++ js/script.js
@@ -46,4 +46,4 @@ funcionamento.init();
 
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
 
-fetchAnimais('../animaisapi.json', '.numeros-grid');
+fetchAnimais('./animaisapi.json', '.numeros-grid');
